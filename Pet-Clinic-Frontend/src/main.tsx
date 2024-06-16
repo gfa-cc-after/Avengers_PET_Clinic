5cad397cb11f3518bb62ed354cc5eca0c1457845
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-06-16 16:32:38 +0200

    added registration page with form

--- Pet-Clinic-Frontend/src/main.tsx
+++ Pet-Clinic-Frontend/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
+import Registration from './pages/Registration';
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
