8ae34f3867cc12c37da28db0ce0e97f3b0d484ab
shruska <147146467+shruska@users.noreply.github.com>
2024-07-10 22:48:57 +0200

    add validateForm, handleSubmit, error message

--- Pet-Clinic-Frontend/src/pages/Registration.tsx
+++ Pet-Clinic-Frontend/src/pages/Registration.tsx
@@ -4,7 +4,7 @@ import './Registration.css';
 const Registration = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null); //generic type <string | null> specify that the state managed by useState can be a string or null. Initial State (null)
     const [submitted, setSubmitted] = useState(false);
 
     const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,10 +19,26 @@ const Registration = () => {
 
     const validateForm = () => {
         if (!email || !password) {
+            //If email is an empty string, null, or undefined, !email will be true.
+            // When email is evaluated, if email is an empty string (""), null, or undefined, it is considered falsy. 
             setError("Email and Password are required");
             return false;
         }
-        if (!/\S+@\S+\.\S+/.test(email)) {
+        if (!/\S+@\S+\.\S+/.test(email)) {  //.test(email): This method tests whether the email string matches the regular expression pattern. It returns true if the string matches and false otherwise.
+            /*             / and /: These are delimiters that indicate the start and end of the regular expression.
+            
+                            \S +:
+            
+                            \S: This matches any non - whitespace character.It is equivalent to[^\s].
+                            +: This quantifier means "one or more" of the preceding token.So, \S + matches one or more non - whitespace characters.
+                            @: This matches the literal @character.
+            
+                            \S +: Same as above, matches one or more non - whitespace characters.This part is used for the domain part before the dot.
+            
+                            \.:
+            
+                            \.: This matches the literal dot.character.The backslash \ is used to escape the dot because, in regular expressions, a dot normally matches any character.
+                            \S +: Again, matches one or more non - whitespace characters.This part is used for the domain extension(e.g., com, net, etc.). */
             setError("Email address is invalid");
             return false;
         }
