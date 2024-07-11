fb0dbdb6c4d67849a4bbf5333350e8a788013f62
Rene Vajda <129347078+ReneVajda1@users.noreply.github.com>
2024-07-11 22:17:54 +0200

    Update Backend-API/src/main/java/com/avangers/backendapi/DTOs/UserDTO.java
    @AllArgsConstructor added and 
    some code removed

--- Backend-API/src/main/java/com/avangers/backendapi/DTOs/UserDTO.java
+++ Backend-API/src/main/java/com/avangers/backendapi/DTOs/UserDTO.java
@@ -3,23 +3,8 @@ package com.avangers.backendapi.DTOs;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserDTO {
    private String email;
    private String password;
-
-   public String getEmail() {
-      return email;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public String getPassword() {
-      return password;
-   }
-
-   public void setPassword(String password) {
-      this.password = password;
-   }
 }
