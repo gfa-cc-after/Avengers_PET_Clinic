bcbbec6f4a61cee9074ff8c3788fafe99888789a
DenisaRohunova <rohunka@gmail.com>
2024-07-13 10:59:18 +0200

    Changes css of registration and login page

--- Pet-Clinic-Frontend/src/pages/Landing.tsx
+++ Pet-Clinic-Frontend/src/pages/Landing.tsx
@@ -4,9 +4,13 @@ import clinicImage from "../images/petclinic.png";
 function PageView() {
   return (
     <>
-     <img src={clinicImage} alt="Clinic" className="clinic-image" /> 
+   
+    <img src={clinicImage} alt="Clinic" className="clinic-image" /> 
       <h1 className="h1">Pet Clinic</h1>
       <h2 className="h2">Welcome to our fabulous clinic</h2>
+
+    
+    
       
     </>
   );
