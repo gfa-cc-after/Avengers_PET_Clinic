519e8ad262c5684ba0cd9537233510bc7b1ef0a9
Metemi <metelkamichal@gmail.com>
2024-07-18 11:11:11 +0200

    Added Test, that check if User with correct email and password is registered

--- Backend-API/src/test/java/com/avangers/backendapi/controllers/UserControllerTest.java
+++ Backend-API/src/test/java/com/avangers/backendapi/controllers/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.avangers.backendapi.controllers;
 
 import com.avangers.backendapi.DTOs.RegisterUserDTO;
 import com.avangers.backendapi.repositories.UserRepository;
+import com.avangers.backendapi.services.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,15 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
- class UserControllerTest {
+class UserControllerTest {
 
     @MockBean
-    private UserRepository userRepository;
+    private UserServiceImpl userServiceImpl;
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,12 +32,24 @@ import org.springframework.test.web.servlet.MockMvc;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+//      objectMapper is used to convert objects to JSON
     private ObjectMapper objectMapper;
-
+//      setUp method is used to create the objectMapper before each test
     @BeforeEach
     void setUp() {
         objectMapper = new ObjectMapper();
     }
-}
 
+   @Test
+   void shouldRregisterUserWithCorrectNameAndPassword() throws Exception {
+      RegisterUserDTO validUser = new RegisterUserDTO("user@example.com", "Abc123456");
+      given(userServiceImpl.addUser(validUser)).willReturn(ResponseEntity.ok("Registration was successful"));
+
+      // Perform the request and expect 200 OK
+      mockMvc.perform(post("/register")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .content(objectMapper.writeValueAsString(validUser)))
+              .andExpect(status().isOk());
+   }
+}
 
