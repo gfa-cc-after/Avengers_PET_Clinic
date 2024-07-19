dfdabed3093fc630dd990c8b455044fcdfd3b8bc
ReneVajda1 <rene.vajda@yahoo.com>
2024-07-19 21:13:09 +0200

    config package fix

--- Backend-API/src/main/java/com/avangers/backendapi/config/FlywayConfig.java
+++ Backend-API/src/main/java/com/avangers/backendapi/config/FlywayConfig.java
@@ -1,4 +1,4 @@
-package com.avangers.backendapi.configuration;
+package com.avangers.backendapi.config;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
