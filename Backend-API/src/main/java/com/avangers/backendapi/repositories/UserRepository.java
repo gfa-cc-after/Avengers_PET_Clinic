50cc12d13dedf49a04c2276f0b4000f5ff903eaf
DenisaRohunova <rohunka@gmail.com>
2024-08-04 13:51:49 +0200

    added the method to find a user by username

--- Backend-API/src/main/java/com/avangers/backendapi/repositories/UserRepository.java
+++ Backend-API/src/main/java/com/avangers/backendapi/repositories/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     User findByEmail(String email);
+     Optional<User> findByUsername(String username);
 }
