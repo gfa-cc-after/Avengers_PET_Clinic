1de427dd7a191e8368e7f5497a08b9c2485b5e7d
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-08-04 21:35:08 +0200

    fix: added semicolon

--- Backend-API/src/main/java/com/avangers/backendapi/services/UserServiceImpl.java
+++ Backend-API/src/main/java/com/avangers/backendapi/services/UserServiceImpl.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
