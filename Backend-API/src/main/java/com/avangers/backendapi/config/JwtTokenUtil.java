10743eabf3cd000d3d06167c1aa502d31b6434f3
ReneVajda1 <rene.vajda@yahoo.com>
2024-08-04 22:15:37 +0200

    JwtRequest class created

--- Backend-API/src/main/java/com/avangers/backendapi/config/JwtTokenUtil.java
+++ Backend-API/src/main/java/com/avangers/backendapi/config/JwtTokenUtil.java
@@ -1,10 +1,11 @@
+package com.avangers.backendapi.config;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
