bab647aced1fd78fffe6dabf5b221c5e061bcb9c
Juraj Trgina <trgina01@google.com>
2024-08-05 15:28:59 +0200

    import jsonwebtoken.security.Keys to JwtTokenUtil class

--- Backend-API/src/main/java/com/avangers/backendapi/config/JwtTokenUtil.java
+++ Backend-API/src/main/java/com/avangers/backendapi/config/JwtTokenUtil.java
@@ -4,6 +4,7 @@ import com.avangers.backendapi.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
