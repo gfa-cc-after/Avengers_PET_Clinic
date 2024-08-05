9796c8f84aefab640a381311ea72b3988228b59e
Michal Metelka <metelkamichal@gmail.com>
2024-08-05 18:06:58 +0200

    controller work with UserResponseDTO

--- Backend-API/src/main/java/com/avangers/backendapi/services/UserService.java
+++ Backend-API/src/main/java/com/avangers/backendapi/services/UserService.java
@@ -1,11 +1,12 @@
 package com.avangers.backendapi.services;
 
 import com.avangers.backendapi.DTOs.RegisterUserDTO;
+import com.avangers.backendapi.DTOs.UserResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-    String addUser(RegisterUserDTO registerUserDTO);
-    String updateUser(Long userId, RegisterUserDTO registerUserDTO);
+    UserResponseDTO addUser(RegisterUserDTO registerUserDTO);
+    UserResponseDTO updateUser(Long userId, RegisterUserDTO registerUserDTO);
 
 }
--- Backend-API/src/main/java/com/avangers/backendapi/services/UserServiceImpl.java
+++ Backend-API/src/main/java/com/avangers/backendapi/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.avangers.backendapi.services;
 
 import com.avangers.backendapi.DTOs.RegisterUserDTO;
+import com.avangers.backendapi.DTOs.UserResponseDTO;
 import com.avangers.backendapi.models.User;
 import com.avangers.backendapi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,9 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public String addUser(RegisterUserDTO registerUserDTO) {
+    public UserResponseDTO addUser(RegisterUserDTO registerUserDTO) {
         if (userRepository.existsByEmail(registerUserDTO.email())) { // Correct accessor
-            return "The email already exists";
+            return new UserResponseDTO(registerUserDTO.email(),"The email already exists");
         }
 
         User newUser = new User();
@@ -29,26 +30,26 @@ public class UserServiceImpl implements UserService {
         newUser.setPassword(passwordEncoder.encode(registerUserDTO.password())); // Correct accessor
         userRepository.save(newUser);
 
-        return "Registration was successful";
+        return new UserResponseDTO(registerUserDTO.email(),"Registration was successful");
     }
 
     @Override
-    public String updateUser(Long userId, RegisterUserDTO registerUserDTO) {
+    public UserResponseDTO updateUser(Long userId, RegisterUserDTO registerUserDTO) {
         User existingUser = userRepository.findById(userId).orElse(null);
 //        check if User exists
         if (existingUser == null) {
-            return "User not found";
+            return new UserResponseDTO(registerUserDTO.email(),"User not found");
         }
 //        check if new email is not already used by another user
         if (!existingUser.getEmail().equals(registerUserDTO.email()) && userRepository.existsByEmail(registerUserDTO.email())) {
-            return "The email already exist";
+            return new UserResponseDTO(registerUserDTO.email(),"The email already exist");
         }
 
         existingUser.setEmail(registerUserDTO.email());
         existingUser.setPassword(passwordEncoder.encode(registerUserDTO.password()));
         userRepository.save(existingUser);
 
-        return "Update was successful";
+        return new UserResponseDTO(registerUserDTO.email(), "Update was successful");
     }
 
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
