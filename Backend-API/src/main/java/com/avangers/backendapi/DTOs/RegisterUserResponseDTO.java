24a816781996de32ac1a632bddd58a983df37e07
Michal Metelka <metelkamichal@gmail.com>
2024-08-07 21:20:48 +0200

    remove @Getter from DTO and wrote back CrossOrigin

--- Backend-API/src/main/java/com/avangers/backendapi/DTOs/RegisterUserResponseDTO.java
+++ Backend-API/src/main/java/com/avangers/backendapi/DTOs/RegisterUserResponseDTO.java
@@ -2,7 +2,7 @@ package com.avangers.backendapi.DTOs;
 
 import lombok.Getter;
 
-@Getter
+
 public record RegisterUserResponseDTO(String email, String message) {
 
 }
--- Backend-API/src/main/java/com/avangers/backendapi/controllers/UserController.java
+++ Backend-API/src/main/java/com/avangers/backendapi/controllers/UserController.java
@@ -8,12 +8,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173/")
 public class UserController {
 
     private final UserService userService;
