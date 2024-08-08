56c8be6edb2bbcaf4086e5c9069dabf43d996c48
Juraj Trgina <trgina01@google.com>
2024-08-08 19:21:58 +0200

    Implement AuthEntryPointJwt to handle unauthorized access errors

---
+++ Backend-API/src/main/java/com/avangers/backendapi/exception/AuthEntryPointJwt.java
@@ -0,0 +1,21 @@
+package com.avangers.backendapi.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Log4j2
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized access error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access error");
+    }
+}
