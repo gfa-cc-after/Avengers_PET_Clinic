c22c983c74e9125fe9a8d5249ac72bc6bb78c61b
Juraj Trgina <trgina01@google.com>
2024-08-08 20:51:08 +0200

    Implement AuthJwtTokenFilter for JWT validation and user authentication

--- Backend-API/src/main/java/com/avangers/backendapi/filter/AuthJwtTokenFilter.java
+++ Backend-API/src/main/java/com/avangers/backendapi/filter/AuthJwtTokenFilter.java
@@ -1,13 +1,18 @@
 package com.avangers.backendapi.filter;
 
-import com.avangers.backendapi.models.User;
+import com.avangers.backendapi.services.UserServiceImpl;
 import com.avangers.backendapi.util.JwtTokenUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -17,11 +22,36 @@ import java.io.IOException;
 public class AuthJwtTokenFilter extends OncePerRequestFilter {
 
     private JwtTokenUtil jwtTokenUtil;
-    private User user;
+    private UserServiceImpl userService;
 
-    public AuthJwtTokenFilter(JwtTokenUtil jwtTokenUtil, User )
+    public AuthJwtTokenFilter(JwtTokenUtil jwtTokenUtil, UserServiceImpl userService) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.userService = userService;
+    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = extractJwtToken(request);
+            if (token != null && jwtTokenUtil.validateJwtToken(token)) {
+                String email = jwtTokenUtil.getEmailFromJwtToken(token);
+                UserDetails userDetails = userService.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken upat =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(upat);
+            }
+        } catch (Exception e) {
+            log.error("User authentication cannot be performed: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
 
+    private String extractJwtToken(HttpServletRequest httpServletRequest){
+        //Bearer egfheghgkehgwhgkgwhkghwergl
+        String header = httpServletRequest.getHeader("Authorization");
+        if(StringUtils.hasText(header) && header.startsWith("Bearer ")){
+            return header.substring(7); //Bearer+space=7, then token
+        }
+        return null;
     }
 }
