e7043477bae3073c982b7f1c9815048b3d8627ca
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-09-05 19:08:09 +0200

    user can add new pet to db

--- Pet-Clinic-Frontend/src/components/PetsList.tsx
+++ Pet-Clinic-Frontend/src/components/PetsList.tsx
@@ -1,55 +1,8 @@
-import { useEffect, useState } from "react";
-import { Pet } from "./Pet";
-
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
-type Pet = {
-  id: number;
-  name: string;
-  type: string;
-};
-
 export const PetsList = () => {
-  const [pets, setPets] = useState<Pet[]>([]);
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await fetch(`${BASE_URL}/api/pets/my-pets`);
-  //     const pets = (await response.json()) as Pet[];
-  //     setPets(pets);
-  //   };
-
-  //   fetchPosts();
-  // }, []);
-
-  useEffect(() => {
-    const headers = { Authorization: "Bearer my-token" };
-    fetch(`${BASE_URL}/api/pets/my-pets`, { headers })
-      .then((response) => response.json())
-      .then((data) => setPets(data));
-  }, []);
-
   return (
     <>
       <h2>Peeeeeeeeeets everywhere!!!!</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pets.map((pet) => (
-            <tr key={pet.id}>
-              <th>{pet.id}</th>
-              <th>{pet.name}</th>
-              <th>{pet.type}</th>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h4>loading...</h4>
     </>
   );
 };
