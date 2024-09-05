df8be9db8d608c75b74a17617f38b76f8b82901e
Juraj Trgina <trgina01@google.com>
2024-09-05 15:50:37 +0200

    create EmailVerification class in models

---
+++ Backend-API/src/main/java/com/avangers/backendapi/models/EmailVerification.java
@@ -0,0 +1,20 @@
+package com.avangers.backendapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+@Data
+@Entity
+@Table(name = "email_verifications")
+@NoArgsConstructor
+public class EmailVerification {
+    @Id
+    @GeneratedValue(generator = "UUID_GENERATOR")
+    @GenericGenerator(name = "UUID_GENERATOR", strategy = "org.hibernate.id.UUIDGenerator")
+    private String verificationId;
+    private String email;
+}
