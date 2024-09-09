1fa201b6b727da6127be7c33beafccc7135a62c0
Juraj Trgina <trgina01@google.com>
2024-09-09 22:50:26 +0200

    fix flyway error

--- Backend-API/src/main/resources/db/migration/V1__create_table_users.sql
+++ Backend-API/src/main/resources/db/migration/V1__create_table_users.sql
@@ -3,5 +3,4 @@ CREATE TABLE IF NOT EXISTS users
     id   SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-    verified SMALLINT(1)
 );
--- Backend-API/src/main/resources/db/migration/V4__create_table_email_verifications.sql
+++ Backend-API/src/main/resources/db/migration/V4__create_table_email_verifications.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS email_verifications
     verification_id VARCHAR (50),
     email VARCHAR (255),
     PRIMARY KEY (verification_id)
-)
+);
