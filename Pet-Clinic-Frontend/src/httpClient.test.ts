2815ee2e6b6224839ca95bc1106730183af09a5a
shruska <147146467+shruska@users.noreply.github.com>
2024-09-12 19:33:34 +0200

    npx biome format ./src --write

--- Pet-Clinic-Frontend/src/httpClient.test.ts
+++ Pet-Clinic-Frontend/src/httpClient.test.ts
@@ -1,18 +1,16 @@
-import { describe, expect, it, vi } from "vitest";
-import { register, } from "./httpClient";
+import { describe, expect, it, vi } from "vitest"
+import { register } from "./httpClient"
 
 vi.mock("./httpClient", () => ({
   register: vi.fn(),
-}));
+}))
 
 describe("httpClient", () => {
-
   it("should call register", async () => {
     await register({
       email: "test",
       password: "test",
-    });
-    expect(register).toHaveBeenCalledTimes(1);
-  });
-
-});
+    })
+    expect(register).toHaveBeenCalledTimes(1)
+  })
+})
