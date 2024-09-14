a5241ed879ac4a92fc13f2dfd4740c93dbbe78f0
markkovari <kovarimarkofficial@gmail.com>
2024-09-14 11:56:59 +0200

    refactor: make User class abstract to prevent instantiations

--- Backend-API/src/main/java/com/avangers/backendapi/models/User.java
+++ Backend-API/src/main/java/com/avangers/backendapi/models/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Getter
 @Setter
 @SuperBuilder
-public class User implements UserDetails {
+public abstract class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
