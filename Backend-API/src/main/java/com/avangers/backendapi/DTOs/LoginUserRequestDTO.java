3de4054bf85c3c088510c7ec774891339bf44375
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-09-19 15:58:12 +0200

    Update LoginUserRequestDTO.java

--- Backend-API/src/main/java/com/avangers/backendapi/DTOs/LoginUserRequestDTO.java
+++ Backend-API/src/main/java/com/avangers/backendapi/DTOs/LoginUserRequestDTO.java
@@ -3,7 +3,7 @@ package com.avangers.backendapi.DTOs;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginUserRequestDTO(
+public record LoginUserRequestDTO (
         @Email String email,
         @NotBlank String password) {
 }
