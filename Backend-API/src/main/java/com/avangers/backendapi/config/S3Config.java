9d748d3a21a6982b8b850650737af935ba6db01e
ReneVajda1 <rene.vajda@yahoo.com>
2024-09-21 02:19:06 +0200

    s3config created + services @Service anotation and builder fix

--- Backend-API/src/main/java/com/avangers/backendapi/config/S3Config.java
+++ Backend-API/src/main/java/com/avangers/backendapi/config/S3Config.java
@@ -1,8 +1,9 @@
 package com.avangers.backendapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
-
+@Configuration
 public class S3Config {
     @Bean
     public S3Client s3Client(){
--- Backend-API/src/main/java/com/avangers/backendapi/services/S3Service.java
+++ Backend-API/src/main/java/com/avangers/backendapi/services/S3Service.java
@@ -1,22 +1,27 @@
 package com.avangers.backendapi.services;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
+import java.nio.file.Paths;
 
+@Service
 public class S3Service {
 
-    private final S3Service s3Service;
+    private final S3Client s3Client;
 
-    public S3Service(S3Service s3Service) {
-        this.s3Service = s3Service;
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
-    public void uploadFile(String bucketName, String keyname, File file) {
+    public void uploadFile(String bucketName, String keyName, File file) {
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(keyname)
+                .key(keyName)
                 .build();
 
+        s3Client.putObject(putObjectRequest, Paths.get(file.getPath()));
     }
 }
